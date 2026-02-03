8cd5866af30085d8a1e5b51df2485714fb8c2f3e
Hosszú Szilárd <55355512+mentol555@users.noreply.github.com>
2026-02-03 16:18:57 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,12 +4,12 @@
 
 const CONFIG = {
     // Your Valentine's name that will appear in the title
-    // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Jade",
+    
+    valentineName: "Bogi",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "Will You Be My Valentine? 💝",
+    pageTitle: "Leszel a Valentin Bixim? 💝",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
@@ -22,10 +22,10 @@ const CONFIG = {
     // Customize each question and its possible responses
     questions: {
         first: {
-            text: "Do you like me?",                                    // First interaction
-            yesBtn: "Yes",                                             // Text for "Yes" button
-            noBtn: "No",                                               // Text for "No" button
-            secretAnswer: "I don't like you, I love you! ❤️"           // Secret hover message
+            text: "Leszel a Valentin Bixim?",                                    // First interaction
+            yesBtn: "YES",                                             // Text for "Yes" button
+            noBtn: "NO",                                               // Text for "No" button
+            secretAnswer: "kivanlak"           // Secret hover message
         },
         second: {
             text: "How much do you love me?",                          // For the love meter
@@ -33,7 +33,7 @@ const CONFIG = {
             nextBtn: "Next ❤️"                                         // Text for the next button
         },
         third: {
-            text: "Will you be my Valentine on February 14th, 2025? 🌹", // The big question!
+            text: "Leszel a Valentin Bixim 2026ban is?", // The big question!
             yesBtn: "Yes!",                                             // Text for "Yes" button
             noBtn: "No"                                                 // Text for "No" button
         }
